6eeac95216dd319c1a785a1f364ec59a93edebb3
zehrayild <230513330@ogr.altinbas.edu.tr>
2026-01-13 14:53:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.textContent = "☀️ Light Mode";
+    } else {
+        themeBtn.textContent = "🌙 Dark Mode";
+    }
+});
